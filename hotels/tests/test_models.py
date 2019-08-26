1b09c5b7b47f7a3b15fa39a0af72126f856d89aa
Timothy <te25son@gmail.com>
2019-08-26 15:11:22 +0200

    Add tests for models

---
+++ hotels/tests/test_models.py
@@ -0,0 +1,71 @@
+from django.test import TestCase
+
+from ..models import City, Hotel
+
+
+class CityModelTests(TestCase):
+
+    def setUp(self):
+        City.objects.create(abbrv='NYC', name='New York')
+
+    def test_city_created(self):
+        city = City.objects.get(name='New York')
+        self.assertTrue(isinstance(city, City))
+        self.assertEquals(city.name, 'New York')
+        self.assertEquals(city.abbrv, 'NYC')
+        self.assertEquals(city.__str__(), city.name)
+
+    def test_city_with_non_unique_abbrv_not_created(self):
+        try:
+            city = City.objects.create(abbrv='NYC', name='New York City')
+        except:
+            city = None
+        self.assertFalse(isinstance(city, City))
+
+    def test_city_with_non_unique_name_not_created(self):
+        try:
+            city = City.objects.create(abbrv="ABC", name="New York")
+        except:
+            city = None
+        self.assertFalse(isinstance(city, City))
+
+
+class HotelModelTests(TestCase):
+
+    def setUp(self):
+        Hotel.objects.create(
+            city=City.objects.create(abbrv='NYC', name='New York'),
+            loc='NYC99',
+            name='The Plaza',
+        )
+
+    def test_hotel_created(self):
+        hotel = Hotel.objects.get(name='The Plaza')
+        self.assertTrue(isinstance(hotel, Hotel))
+        self.assertEquals(hotel.city.name, 'New York')
+        self.assertEquals(hotel.city.abbrv, 'NYC')
+        self.assertEquals(hotel.loc, 'NYC99')
+        self.assertEquals(hotel.name, 'The Plaza')
+        self.assertEquals(hotel.__str__(), 'The Plaza')
+
+    def test_hotel_with_same_name_and_city_but_diff_loc_created(self):
+        hotel = Hotel.objects.create(
+            city=City.objects.get(name='New York'),
+            loc='NYC01',
+            name='The Plaza',
+        )
+        self.assertTrue(isinstance(hotel, Hotel))
+
+    def test_hotel_with_non_unique_loc_and_name_not_created(self):
+        try:
+            hotel = Hotel.objects.create(
+                city=City.objects.create(
+                    abbrv='AMS',
+                    name='Amsterdam',
+                ),
+                loc='NYC99',
+                name='The Plaza',
+            )
+        except:
+            hotel = None
+        self.assertFalse(isinstance(hotel, Hotel))
