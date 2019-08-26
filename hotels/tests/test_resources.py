39d7f4a0bb3ca7fb3f5fd3d6f8350d4a292a9702
Timothy <te25son@gmail.com>
2019-08-26 18:08:19 +0200

    Add tests for CityResource

---
+++ hotels/tests/test_resources.py
@@ -0,0 +1,44 @@
+import tablib
+
+from django.test import TestCase
+
+from ..resources import CityResource, HotelResource
+from ..models import City, Hotel
+
+
+class CityResourceTests(TestCase):
+
+    def setUp(self):
+        self.resource = CityResource()
+        self.dataset = tablib.Dataset(
+            headers=['test-abbrv', 'test-name']
+        )
+
+    def test_fields(self):
+        fields = self.resource.fields
+        self.assertIn('abbrv', fields)
+        self.assertIn('name', fields)
+
+    def test_excluded_fields(self):
+        fields = self.resource.fields
+        self.assertNotIn('id', fields)
+
+    def test_before_import(self):
+        self.resource.before_import(
+            self.dataset
+        )
+        self.assertEquals(self.dataset.headers, ['abbrv', 'name'])
+        self.assertEquals(self.dataset.height, 1)  # Original headers should have been appended to the dataset making the height one not including the new headers
+        self.assertEquals(self.dataset['abbrv'], ['test-abbrv'])
+        self.assertEquals(self.dataset['name'], ['test-name'])
+
+    def test_import_data(self):
+        imported_data = self.resource.import_data(
+            self.dataset,
+            raise_errors=True
+        )
+        self.assertFalse(imported_data.has_errors())
+        self.assertEquals(len(imported_data.rows), 1)
+
+        imported_object = City.objects.get(name='test-name')
+        self.assertEquals(imported_object.abbrv, 'test-abbrv')
