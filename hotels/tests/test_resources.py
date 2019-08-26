8b627e824b5ce8b832685a46887c0ea3f81b4496
Timothy <te25son@gmail.com>
2019-08-26 19:20:09 +0200

    Add tests for HotelResource

--- hotels/tests/test_resources.py
+++ hotels/tests/test_resources.py
@@ -42,3 +42,47 @@ class CityResourceTests(TestCase):
 
         imported_object = City.objects.get(name='test-name')
         self.assertEquals(imported_object.abbrv, 'test-abbrv')
+
+
+class HotelResourceTests(TestCase):
+
+    def setUp(self):
+        self.resource = HotelResource()
+        self.dataset = tablib.Dataset(
+            headers=['test-city-abbrv', 'test-loc', 'test-name']
+        )
+
+    def test_fields(self):
+        fields = self.resource.fields
+        self.assertIn('city', fields)
+        self.assertIn('loc', fields)
+        self.assertIn('name', fields)
+
+    def test_excluded_fields(self):
+        fields = self.resource.fields
+        self.assertNotIn('id', fields)
+
+    def test_before_import(self):
+        self.resource.before_import(
+            self.dataset
+        )
+        self.assertEquals(self.dataset.headers, ['city', 'loc', 'name'])
+        # Original headers should have been appended to the dataset making the height one not including the new headers
+        self.assertEquals(self.dataset.height, 1)
+        self.assertEquals(self.dataset['city'], ['test-city-abbrv'])
+        self.assertEquals(self.dataset['loc'], ['test-loc'])
+        self.assertEquals(self.dataset['name'], ['test-name'])
+
+    def test_import_data(self):
+        # City object must be imported before foreignkey can be paired
+        city = City.objects.create(abbrv='test-city-abbrv', name='test-city-name')
+        # Now import data for Hotel
+        imported_data = self.resource.import_data(
+            self.dataset,
+            raise_errors=True
+        )
+        self.assertFalse(imported_data.has_errors())
+        self.assertEquals(len(imported_data.rows), 1)
+
+        imported_object = Hotel.objects.get(name='test-name')
+        self.assertEquals(imported_object.city, city)
