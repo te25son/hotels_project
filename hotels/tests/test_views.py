dc98e96654dd58d24bafae9ac6fb8bb021d58466
Timothy <te25son@gmail.com>
2019-08-27 14:49:55 +0200

    Add further views tests

--- hotels/tests/test_views.py
+++ hotels/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 
 from ..views import CitySearchTemplateView, HotelListView
+from ..models import City, Hotel
 
 
 class CitySearchTemplateViewTests(TestCase):
@@ -13,15 +14,59 @@ class CitySearchTemplateViewTests(TestCase):
     def test_status_code(self):
         self.assertEquals(self.response.status_code, 200)
 
+    def test_uses_correct_view_class(self):
+        view = resolve('/')
+        self.assertEquals(view.func.view_class, CitySearchTemplateView)
+
     def test_uses_correct_template(self):
         self.assertEquals(self.response.template_name, ['search.html'])
+
+    def test_view_has_input(self):
+        self.assertContains(self.response, '<input', 1)
 
 
 class HotelListViewTests(TestCase):
 
     def setUp(self):
         self.url = reverse('hotel_results')
+        self.response = self.client.get(self.url)
+        city = City.objects.create(
+            abbrv='NYC',
+            name='New York',
+        )
+        hotel = Hotel.objects.create(
+            city=city,
+            loc='NYC99',
+            name='The Plaza',
+        )
 
     def test_status_code(self):
         self.assertEquals(self.response.status_code, 200)
-        print(self.response.content)
+
+    def test_uses_correct_view_class(self):
+        view = resolve('/hotels/')
+        self.assertEquals(view.func.view_class, HotelListView)
+
+    def test_query_objects_with_data(self):
+        response = self.client.get(self.url, {'q': 'New York'})
+        self.assertContains(response, 'Hotels in New York')
+        self.assertContains(response, '<table')
+        self.assertContains(response, 'The Plaza')
+        self.assertContains(response, 'NYC99')
+        self.assertContains(response, 'Oh no!', 0) # does not contain
+
+    def test_query_objects_without_data(self):
+        response = self.client.get(self.url, {'q': 'Amsterdam'})
+        self.assertContains(response, 'Hotels in Amsterdam')
+        self.assertContains(response, '<table', 0) # does not contain
+        self.assertContains(response, 'Oh no!')
+
+    def test_blank_query_object(self):
+        response = self.client.get(self.url, {'q': ''})
+        self.assertContains(response, 'Hotels')
+        self.assertContains(response, 'Hotels in', 0)
+        self.assertContains(response, '<table')  # returns all hotels in database
+
+    def test_has_link_back_to_seach_view(self):
+        search_view = reverse('search')
+        self.assertContains(self.response, f'href="{search_view}"')
