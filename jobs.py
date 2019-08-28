87a74613b8a410053474acc2ba1158372078934c
Timothy <te25son@gmail.com>
2019-08-28 11:57:55 +0200

    Scheduler working, change seconds to minutes

--- jobs.py
+++ jobs.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.background import BlockingScheduler
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', seconds=30)
+@scheduler.scheduled_job('interval', minutes=30)
 def subprocess_to_update_models_from_csv():
     subprocess.call('python manage.py jobs', shell=True, close_fds=True)
 
